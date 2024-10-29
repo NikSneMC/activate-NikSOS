add9c23f195a41c8839bc79528d2ae5ddbdfae55
Trouble-Truffle <perigordtruffle7318@gmail.com>
2024-10-29 18:29:59 +0800

    doc: Add meta items to default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ rustPlatform, libxkbcommon, pkg-config }:
+{ lib, rustPlatform, libxkbcommon, pkg-config }:
 rustPlatform.buildRustPackage {
   pname = "activate-linux";
   version = "0.1.0";
@@ -6,4 +6,12 @@ rustPlatform.buildRustPackage {
   cargoHash = "sha256-TtTcYEuNPd25j7raC0uengUNb44O7L52IVBWAYxHAuU=";
   buildInputs = [ libxkbcommon ];
   nativeBuildInputs = [ pkg-config ];
+
+  meta = with lib; {
+    description = ''Windows' "Active Windows" watermark for Linux '';
+    homepage = "https://github.com/Perigord-Kleisli/activate-linux";
+    mainProgram = "activate-linux";
+    platforms = platforms.linux;
+    license = licenses.mit;
+  };
 }
